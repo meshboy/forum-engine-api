b5d3f5ce71c2db8d49c37759291c3e74ff4b41c2
meshileya seun <meshileyaseun@gmail.com>
2018-04-08 22:47:00 +0100

    make package.json more descriptive

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-/**
- *created by Meshileya Seun <meshileyaseun@gmail.com/> 4/6/18
- **/
 const express = require('express')
 
+// declare an app from express
 const app = express()
 
+// create a basic index route
 app.get('/', (req, res) => {
 
   res.json({ status: 'Introduction to Nodejs' })
 })
 
+// start the server on the assigned port
 app.listen(3000, () => {
   console.log('I am listening to port 3000')
 })
